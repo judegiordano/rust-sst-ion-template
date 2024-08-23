b9ee7c4ba1d1332bc27a240ed93ab7822f0c37c6
judegiordano <judegiordano@gmail.com>
2024-08-23 19:50:38 -0400

    sst init

---
+++ sst.config.ts
@@ -0,0 +1,36 @@
+
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "rust-sst-ion-template",
+      removal: "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          region: 'us-east-1'
+        }
+      },
+      stage: input?.stage
+    };
+  },
+  async run() {
+    const { stage } = $app;
+
+    const api = new sst.aws.Function("api", {
+      handler: "bootstrap",
+      architecture: "arm64",
+      timeout: "60 seconds",
+      bundle: "./target/lambda/api",
+      runtime: 'provided.al2023',
+      url: true,
+      environment: {
+        STAGE: stage
+      }
+    });
+    return {
+      url: api.url
+    }
+  },
+});
