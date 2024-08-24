a2ad2fbc218838713fce1221cdb1e8a004e00cd2
judegiordano <judegiordano@gmail.com>
2024-08-24 13:15:18 -0400

    cache example

--- src/controllers/dev/controller.rs
+++ src/controllers/dev/controller.rs
@@ -1,18 +1,24 @@
-use axum::{response::IntoResponse, Json};
-use serde::Serialize;
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use meme_cache::{get, set};
+use serde::{Deserialize, Serialize};
 
 use crate::{
     env::{Env, Stage},
     types::ApiResponse,
 };
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct PingMessage {
     ok: bool,
     stage: Stage,
 }
 
 pub async fn ping() -> ApiResponse {
+    if let Some(exists) = get::<PingMessage>("ping").await {
+        return Ok(Json(exists).into_response());
+    }
     let Env { stage, .. } = Env::load()?;
-    Ok(Json(PingMessage { ok: true, stage }).into_response())
+    let response = PingMessage { ok: true, stage };
+    set("ping", &response, 10_000).await;
+    Ok((StatusCode::CREATED, Json(response)).into_response())
 }
