1ab97163c575ecb75430c2a995eb1bdcd9b8f8bb
judegiordano <judegiordano@gmail.com>
2024-08-25 13:56:52 -0400

    add log level to env / base args transformer for functions

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,12 @@
 
-/// <reference path="./.sst/platform/config.d.ts" />
+/// <reference path='./.sst/platform/config.d.ts' />
 
 export default $config({
   app(input) {
     return {
-      name: "rust-sst-ion-template",
-      removal: "remove",
-      home: "aws",
+      name: 'rust-sst-ion-template',
+      removal: 'remove',
+      home: 'aws',
       providers: {
         aws: {
           region: 'us-east-1'
@@ -17,22 +17,23 @@ export default $config({
   },
   async run() {
     const { stage } = $app;
-
-    const api = new sst.aws.Function("api", {
-      handler: "bootstrap",
-      architecture: "arm64",
-      bundle: "target/lambda/api",
-      runtime: 'provided.al2023',
-      timeout: "10 minutes",
-      memory: "500 MB",
-      url: {
-        cors: true,
-        allowCredentials: true
-      },
-      environment: {
+    $transform(sst.aws.Function, (args, _) => {
+      args.handler = 'bootstrap';
+      args.architecture = 'arm64';
+      args.runtime = 'provided.al2023';
+      args.timeout = '10 minutes';
+      args.memory = '500 MB';
+      args.environment = {
         STAGE: stage,
+        LOG_LEVEL: process.env.MONGO_URI,
         MONGO_URI: process.env.MONGO_URI
-      }
+      };
+    })
+
+    const api = new sst.aws.Function('api', {
+      handler: 'bootstrap',
+      bundle: 'target/lambda/api',
+      url: { cors: true, allowCredentials: true },
     });
     return {
       url: api.url
